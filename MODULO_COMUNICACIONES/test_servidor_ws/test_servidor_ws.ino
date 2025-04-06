b0bc89060f3e54145b4d446f3d24ab7548aeaf0b
SMBTime <126691426+SMBTime@users.noreply.github.com>
2025-04-06 23:36:06 +0200

    Update test_servidor_ws.ino

--- MODULO_COMUNICACIONES/test_servidor_ws/test_servidor_ws.ino
+++ MODULO_COMUNICACIONES/test_servidor_ws/test_servidor_ws.ino
@@ -26,6 +26,21 @@ uint32_t sharedKey;
 
 bool claveIntercambiada = false;
 
+//Metodo para generar la clave pública.
+uint32_t modexp(uint32_t base, uint32_t exp, uint32_t mod) {
+    uint32_t result = 1;
+    base = base % mod;
+
+    while (exp > 0) {
+        if (exp % 2 == 1)  // If exp is odd
+            result = (uint64_t)result * base % mod;
+
+        exp = exp >> 1; // exp = exp / 2
+        base = (uint64_t)base * base % mod;
+    }
+    return result;
+}
+
 void setup() {
     Serial.begin(115200);
 
@@ -44,7 +59,7 @@ void setup() {
 
     // Generación de claves con Diffie-Hellman
     privateKey = random(2, 10);
-    publicKey = (uint32_t)pow(g, privateKey) % p;
+    publicKey = modexp(g, privateKey, p);
 
     Serial.print("Clave privada (server): ");
     Serial.println(privateKey);
